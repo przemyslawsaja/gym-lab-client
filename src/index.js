84787e0506131db7496615e0418e073ed4402762
Przemyslaw Saja <przemyslaw@pushpushgo.com>
2022-04-16 19:32:15 +0200

    update service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './pages/Root'
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +11,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
 reportWebVitals();
